8eb68ede8334703a72d03a4b459c101953a82f51
c.rabarijohn <c.rabarijohn@fulldigits.com>
2024-03-16 15:20:14 +0300

    [feat] add working functions

---
+++ libft/libft.h
@@ -0,0 +1,23 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+// Libc functions
+
+int ft_islapha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+size_t ft_strlen(const char *s);
+// void *ft_memset();
+// void *ft_memset(void *s, int c, size_t n);
+
+// Additional functions
+
+// Others
+
+#endif
