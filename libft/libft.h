de7f1195359dafb877c9bca6ee86bf7740f25889
Tiana Cedric Rabarijohn <trabarij@c1r6s10.42antananarivo.mg>
2024-03-17 11:32:38 +0300

    [feat] insert ft_memset in libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -16,6 +16,7 @@
 # include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 // Libc functions
 
@@ -25,9 +26,7 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
-
-// void *ft_memset();
-// void *ft_memset(void *s, int c, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 
 // Additional functions
 
