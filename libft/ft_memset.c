68ffa414a9ee61c39036608f07b027d37f907151
Tiana Cedric Rabarijohn <trabarij@c1r6s10.42antananarivo.mg>
2024-03-17 11:32:25 +0300

    [feat] add ft_memset function

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,13 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: trabarij <marvin@42.fr>                    +#+ +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 17:23:07 by trabarij          #+#    #+#             */
-/*   Updated: 2024/03/16 17:23:07 by trabarij         ###   ########.fr       */
+/*   Updated: 2024/03/17 10:50:10 by trabarij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = (unsigned char)c;
+		i++;
+	}
+	return (str);
+}
