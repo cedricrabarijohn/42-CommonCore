862cc01baf54b370c6932602b22d85346ca741f5
Tiana Cedric Rabarijohn <trabarij@c1r7s6.42antananarivo.mg>
2024-03-23 14:22:05 +0300

    [fix] add NULL checking in ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*tmp_dest;
 	unsigned char	*tmp_src;
 
+	if (!dest && !src)
+		return (NULL);
 	i = 0;
 	tmp_dest = (unsigned char *)dest;
 	tmp_src = (unsigned char *)src;
