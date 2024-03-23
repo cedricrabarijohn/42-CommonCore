6091c725ff6047aeb34b2577328e063d718b2375
Tiana Cedric Rabarijohn <trabarij@c1r7s6.42antananarivo.mg>
2024-03-23 14:23:15 +0300

    [feat] ft_memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -12,7 +12,31 @@
 
 #include "libft.h"
 
-// void *ft_memmove(void *dest, const void *src, size_t n)
-// {
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*tmp_dest;
+	unsigned char	*tmp_src;
 
-// }
+	i = 0;
+	tmp_dest = (unsigned char *)dest;
+	tmp_src = (unsigned char *)src;
+	if (tmp_dest < tmp_src || tmp_dest >= (tmp_src + n))
+	{
+		while (i < n)
+		{
+			*tmp_dest++ = *tmp_src++;
+			i++;
+		}
+	}
+	else
+	{
+		tmp_dest += n;
+		tmp_src += n;
+		while (n--)
+		{
+			*(tmp_dest--) = *(tmp_src--);
+		}
+	}
+	return (dest);
+}
