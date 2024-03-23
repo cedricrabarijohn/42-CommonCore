62df7081f6249c62bbe6e498b2992a06333713d5
Tiana Cedric Rabarijohn <trabarij@c1r7s6.42antananarivo.mg>
2024-03-23 16:13:44 +0300

    [feat] impl ft_strlcpy

--- libft/libft.h
+++ libft/libft.h
@@ -18,7 +18,7 @@
 # include <stdlib.h>
 // Libc functions
 
-int		ft_islapha(int c);
+int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
@@ -28,6 +28,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 // Additional functions
 
 // Others
