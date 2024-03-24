cabc01907bde620848b4d216f52b5d0f5d9534dc
Tiana Cedric Rabarijohn <trabarij@c1r6s12.42antananarivo.mg>
2024-03-24 16:27:10 +0300

    [feat] add ft_toupper function

--- libft/libft.h
+++ libft/libft.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: trabarij <marvin@42.fr>                    +#+ +:+       +#+        */
+/*   By: trabarij <trabarij@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/16 17:44:17 by trabarij          #+#    #+#             */
-/*   Updated: 2024/03/16 17:44:17 by trabarij         ###   ########.fr       */
+/*   Created: 2024/03/24 16:18:31 by trabarij          #+#    #+#             */
+/*   Updated: 2024/03/24 16:26:11 by trabarij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
 // Additional functions
 
 // Others
