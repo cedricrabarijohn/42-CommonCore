b6ba0daa40cbe6f920481f3a63504ad6bf21425a
Tiana Cedric Rabarijohn <trabarij@c1r7s11.42antananarivo.mg>
2024-03-31 18:22:29 +0300

    [feat] init ft_itoa

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: trabarij <trabarij@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 16:18:31 by trabarij          #+#    #+#             */
-/*   Updated: 2024/03/31 15:01:06 by trabarij         ###   ########.fr       */
+/*   Updated: 2024/03/31 18:21:18 by trabarij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,9 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-// changes--
 char	*ft_strtrim(char const *s1, char const *set);
+// changes--
+char	*ft_itoa(int n);
 // Additional functions
 
 // Others
