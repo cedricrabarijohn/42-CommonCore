98ea05c616c6c939653e3c1223a425c61c157faa
c.rabarijohn <c.rabarijohn@fulldigits.com>
2024-04-01 10:40:47 +0300

    [feat] wip ft_itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -12,8 +12,56 @@
 
 #include "libft.h"
 
+static char	nb_to_char(int nb)
+{
+	char	res;
+	res = '0';
+	if (ft_isdigit(nb + 48))
+		res = nb + 48;
+	return (res);
+}
+
+static size_t	get_nb_length(int nb)
+{
+	int		tmp;
+	size_t	ln;
+
+	ln = 1;
+	tmp = nb;
+	while (tmp != 0)
+	{
+		tmp = tmp/10;
+		ln++;
+	}
+	if (nb < 0)
+		ln++;
+	return (ln);
+}
+
 char	*ft_itoa(int n)
 {
-	
-	return ;
+	size_t	nb;
+	size_t	nb_len;
+	size_t	tmp;
+	char	*res;
+	int		i;
+
+	nb_len = get_nb_length(n);
+	nb = (size_t)n;
+	res = (char *)malloc((nb_len + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	if (n < 0)
+		nb = -nb;
+	i = nb_len - 1;
+	while (i--)
+	{
+		tmp = nb % 10;
+		nb = nb / 10;
+		res[i] = nb_to_char(tmp);
+		if(i == 0 && n < 0)
+			res[i] = '-';
+	}
+	res[nb_len] = '\0';
+	return (res);
 }
