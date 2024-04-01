d33cfd401627e8de8f445ac27054866928c3592b
Tiana Cedric Rabarijohn <trabarij@c1r7s11.42antananarivo.mg>
2024-04-01 13:07:52 +0300

    [feat] finish ft_strrchr

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: trabarij <trabarij@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 16:18:31 by trabarij          #+#    #+#             */
-/*   Updated: 2024/03/31 18:21:18 by trabarij         ###   ########.fr       */
+/*   Updated: 2024/04/01 12:40:56 by trabarij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
