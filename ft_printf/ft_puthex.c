247085dcf91a161d0024b4b3f867f9ff3097017d
Tiana Cedric Rabarijohn <trabarij@c1r7s11.42antananarivo.mg>
2024-07-16 15:18:19 +0300

    [fix] fix norminette

--- ft_printf/ft_puthex.c
+++ ft_printf/ft_puthex.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trabarij <trabarij@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:59:18 by trabarij          #+#    #+#             */
+/*   Updated: 2024/07/16 15:18:03 by trabarij         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int ft_puthex(unsigned int n, int uppercase)
+int	ft_puthex(unsigned int n, int uppercase)
 {
-    char *base;
-    int len = 0;
+	char	*base;
+	int		len;
 
-    if (uppercase)
-        base = "0123456789ABCDEF";
-    else
-        base = "0123456789abcdef";
-
-    if (n >= 16)
-        len += ft_puthex(n / 16, uppercase);
-    len += ft_putchar(base[n % 16]);
-    return len;
+	len = 0;
+	if (uppercase)
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+	if (n >= 16)
+		len += ft_puthex(n / 16, uppercase);
+	len += ft_putchar(base[n % 16]);
+	return (len);
 }
