d7fe761017bc39b44e5f1e2f349f360eb7af05fe
Tiana Cedric Rabarijohn <trabarij@c1r7s11.42antananarivo.mg>
2024-07-16 15:15:55 +0300

    [feat] add ft_printf.h and fix norminette

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trabarij <trabarij@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 14:59:12 by trabarij          #+#    #+#             */
+/*   Updated: 2024/07/16 15:14:43 by trabarij         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-int ft_printf(const char *format, ...);
-int ft_putchar(char c);
-int ft_putstr(char *s);
-int ft_putnbr(int n);
-int ft_putunbr(unsigned int n);
-int ft_puthex(unsigned int n, int uppercase);
-int ft_putptr(unsigned long long ptr);
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_putunbr(unsigned int n);
+int	ft_puthex(unsigned int n, int uppercase);
+int	ft_putptr(unsigned long long ptr);
 
 #endif
